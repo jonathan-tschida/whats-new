ac5591059d2343c0d3b26a00c2e9ed573cc15be4
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-03-16 20:34:24 -0600

    Create initial NewsContainer structure

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NewsArticle from '../NewsArticle/NewsArticle.js';
 import './NewsContainer.css'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+class NewsContainer extends Component {
+
+  render() {
+    return (
+      <section>
+        {this.props.news.map(story => new NewsArticle(story).render())}
+      </section>
+    );
+  }
+}
 
 export default NewsContainer;
