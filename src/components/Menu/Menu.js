f055103de71a5312fa17d331791fe163e4f6b8ba
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-03-16 20:54:23 -0600

    Refactor Menu render method

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,11 +5,9 @@ class Menu extends Component {
   render() {
     return (
       <nav>
-        <button value="local" onClick={this.props.changeCategory}>Local News</button>
-        <button value="technology" onClick={this.props.changeCategory}>Technology</button>
-        <button value="entertainment" onClick={this.props.changeCategory}>Entertainment</button>
-        <button value="science" onClick={this.props.changeCategory}>Science</button>
-        <button value="health" onClick={this.props.changeCategory}>Health</button>
+        {this.props.categories.map((category, index) => (
+          <button key={index} value={category} onClick={this.props.changeCategory}>{category}</button>
+        ))}
       </nav>
     );
   }
