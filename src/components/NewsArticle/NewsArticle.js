09210f0d07ad55fb4129bc6fe628d241c05df3d0
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-03-16 20:34:53 -0600

    Create initial NewsArticle structure

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+class NewsArticle extends Component {
+  render() {
+    return (
+      <article key={this.props.id}>
+        <h2>{this.props.headline}</h2>
+        <img src={this.props.img} alt='' />
+        <p>{this.props.description}</p>
+        <a href={this.props.url}>Link to article</a>
+      </article>
+    );
+  }
+}
 
 export default NewsArticle;
