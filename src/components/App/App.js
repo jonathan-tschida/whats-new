ba54cef229381cad9506b27fc3a4db65033b43b4
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-03-16 20:33:08 -0600

    Create initial App structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
+import NewsContainer from '../NewsContainer/NewsContainer.js';
+import Menu from '../Menu/Menu.js';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
 import local from '../../data/local';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      appName: 'What\'s New?',
+      selectedCategory: 'local',
+      entertainment,
+      health,
+      local,
+      science,
+      technology
     }
   }
 
+  changeCategory = (event) => {
+    this.setState({selectedCategory: event.target.value});
+  }
+
   render () {
+    const {selectedCategory} = this.state;
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <header>
+          <h1>{this.state.appName}</h1>
+          <Menu changeCategory={this.changeCategory} />
+        </header>
+        <NewsContainer news={this.state[selectedCategory]}/>
       </div>
     );
   }
