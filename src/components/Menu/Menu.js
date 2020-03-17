9ad1d19a3391152c55ba22e8748620584291068e
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-03-16 20:33:43 -0600

    Create initial Menu structure

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+class Menu extends Component {
+  render() {
+    return (
+      <nav>
+        <button value="local" onClick={this.props.changeCategory}>Local News</button>
+        <button value="technology" onClick={this.props.changeCategory}>Technology</button>
+        <button value="entertainment" onClick={this.props.changeCategory}>Entertainment</button>
+        <button value="science" onClick={this.props.changeCategory}>Science</button>
+        <button value="health" onClick={this.props.changeCategory}>Health</button>
+      </nav>
+    );
+  }
+}
 
 export default Menu;
